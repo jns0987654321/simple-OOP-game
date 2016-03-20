0fdd62d22505f5e348ffe6785518c931425dd916
Junior Samaroo <jns0987654321@gmail.com>
2016-03-20 11:56:07 -0400

    Initial Commit

---
+++ simple-OOP-game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple-OOP-game
+//
+//  Created by Junior Samaroo on 2016-03-20.
+//  Copyright © 2016 Junior Samaroo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
