851fef3bd9c2553b6c2ce1363c4302aeaabe3f66
Junior Samaroo <jns0987654321@gmail.com>
2016-03-20 12:18:42 -0400

    Added Player Class

---
+++ simple-OOP-game/Player.swift
@@ -0,0 +1,33 @@
+//
+//  Player.swift
+//  simple-OOP-game
+//
+//  Created by Junior Samaroo on 2016-03-20.
+//  Copyright © 2016 Junior Samaroo. All rights reserved.
+//
+
+import Foundation
+
+class Player: Character {
+    private var _name = "Player"
+    
+    var name: String {
+        get {
+            return _name
+        }
+    }
+    
+    private var _inventory = [String]()
+    
+    var inventory: [String] {
+        return _inventory
+    }
+    
+    //init for inherited class
+    convenience init(name: String, hp: Int, attackPwr: Int){
+        //init for parent class
+        self.init(startingHp: hp, attackPwr: attackPwr)
+        
+        _name = name
+    }
+}
